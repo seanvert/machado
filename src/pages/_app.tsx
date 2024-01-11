0826eaa3b1c7764140a25649e1538217efc9b186
Sean Uchida <seankuchida@gmail.com>
2024-01-11 20:53:40 -0300

    shadcn up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,28 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
+import "src/styles/globals.css"
 
-import "~/styles/globals.css";
 
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
+      <div className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
       <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
---
+++ src/pages/components/navbar.tsx
@@ -0,0 +1,31 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+export const NavBar = () => {
+    return (
+        <AuthShowcase />
+    )
+}
+
+function AuthShowcase() {
+    const { data: sessionData } = useSession();
+  
+    const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+      undefined, // no input
+      { enabled: sessionData?.user !== undefined }
+    );
+  
+    return (
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-center text-2xl bg-slate-200">
+          {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+          {secretMessage && <span> - {secretMessage}</span>}
+        </p>
+        <button
+          className="rounded-full bg-blue-800 px-10 py-3 font-semibold text-white no-underline transition hover:bg-blue-600"
+          onClick={sessionData ? () => void signOut() : () => void signIn()}
+        >
+          {sessionData ? "Sign out" : "Sign in"}
+        </button>
+      </div>
+    );
+  }
