07c8198819451dc89d25ac53daee8e0d3ca07658
Sean Uchida <seankuchida@gmail.com>
2024-01-26 10:45:23 -0300

    changed app name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { api } from "~/utils/api";
 import Image from 'next/image'
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -16,16 +15,20 @@ export default function Home() {
       <main className="
       bg-gradient-radial from-black via-zinc-950 to-black
       flex min-h-screen flex-grow flex-col items-center ">
-      <NavBar />
-        
-    <Image
-          className="" 
+        <NavBar />
+
+        <Image
+          className=""
           src="/Transparent_Machado_de_Assis.png"
           width={350}
           height={350}
           alt="Picture of the author"
-    />
-    <h1 className="-mt-64 z-10 text-[330px] p-4 tracking-tighter font-oswald text-center font-bold bg-gradient-to-br from-violet-800 via-orange-600 to-indigo-700 bg-clip-text text-transparent">machado</h1>
+        />
+        <h1 className="leading-none -mt-52 z-10 text-[330px] p-4 tracking-tighter font-oswald text-center font-bold 
+    bg-gradient-to-tr from-rose-500 via-orange-600 to-fuchsia-400
+    bg-clip-text text-transparent">
+          assis
+        </h1>
       </main>
     </>
   );
