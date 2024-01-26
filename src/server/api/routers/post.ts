ebc93dc316a9c67a7b2c2f1518f3ae8b78ff9166
Sean Uchida <seankuchida@gmail.com>
2024-01-26 10:24:34 -0300

    getAllByUser trpc route

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -36,6 +36,12 @@ export const postRouter = createTRPCRouter({
     });
   }),
 
+  getAllByUser: protectedProcedure.query(({ ctx}) => {
+    return ctx.db.post.findMany({
+      orderBy: { createdAt: "desc" },
+      where: { createdBy: { id: ctx.session.user.id } },
+    })
+  }),
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
