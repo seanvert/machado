237f5036d0f89e3b981c386ae45ecaef07fc0231
Sean Uchida <seankuchida@gmail.com>
2024-02-04 17:50:26 -0300

    increased db exercise and text sizes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -49,7 +49,29 @@ async function main() {
                 }
             },
         })
-
+        const automaticWriting = await prisma.exercise.upsert({
+            where: { id: i + 10 },
+            update: {},
+            create: {
+                name: 'Automatic Writing' ,
+                contents: `From his famed treatise, The Surrealist Manifesto, Breton describes his specific technique for automatic writing: 
+                Find a place as conducive as possible to the concentration of the spirit.\n
+                Enter the most passive, or receptive state of mind possible.\n
+                Dispense with any preoccupations with the genius or talent of others.\n
+                Repeat that literature is one of the saddest roads that lead everywhere.\n
+                Write quickly, without preconceived topic, unable to stop or to be tempted to read what is written.\n
+                Let the first sentence that comes to mind remain and continue like this.\n
+                Trust in the inexhaustible nature of the murmur.\n
+                If silence threatens, due to a \“lack of inattention,\” write any letter, returning to a state of arbitrariness.\n
+                André Bretón defined automatic writing as a way of creating art through pure spontaneous thought, \“free from aesthetic or moral preoccupations.\” This appealed to many freethinkers of the time. He was as charismatic as he was enigmatic, and his following grew.\n`,
+                configs: '{ configs: teste}',
+                progress: '{ progress: { automaticWriting: 100}}',
+                type: i,
+                createdBy: {
+                    connect: { id: user.id },
+                },
+            },
+        })
         // create exercises with new users
         const exercise = await prisma.exercise.upsert({
             where: { id: i + 1 },
